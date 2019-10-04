4ff78a781f1bbb1277bd9f4a6e639e75b9141465
codechris1 <52330846+codechris1@users.noreply.github.com>
2019-10-04 00:08:46 +0000

    a
    a

--- lib/tests/test_main.py
+++ lib/tests/test_main.py
@@ -4,7 +4,8 @@ def input_parameters():
     input_texts={
         'path': 'Provide the Path Location: ',
         'server1': 'Provide the Hostname for Server 1: ',
-        'user1' : 'Provide the User for Server 1: '
+        'user1' : 'Provide the User for Server 1: ',
+        'password1' : 'Provide the Password for Server 1: '
     }
     
     #args['path']=raw_input('Provide the Path Locaton: ')

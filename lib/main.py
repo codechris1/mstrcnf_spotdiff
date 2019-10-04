b76adf0d8606a35ae6590fe8ddf5ba031a7230fd
codechris1 <52330846+codechris1@users.noreply.github.com>
2019-10-04 16:24:58 +0000

    A
    A

--- lib/main.py
+++ lib/main.py
@@ -3,3 +3,24 @@ import sys
 import json
 import utility
 
+def main():
+    args = utility.pick_config()[1]
+
+    #Create Project Sources
+    utility.create_project_source(args['path'],args['server1'],args['port1'],args['user1'],args['password1'])
+    utility.create_project_source(args['path'],args['server2'],args['port2'],args['user2'],args['password2'])
+
+    #Gather Server Configurations
+    server_level1 = utility.rf_cmmgr(args['path'], args['server1'], args['user1'], args['password1'], args['port1'], args['project1'], 'Server')
+    server_level2 = utility.rf_cmmgr(args['path'], args['server2'], args['user2'], args['password2'], args['port2'], args['project2'], 'Server')
+
+    #Gather Project Configurations
+    project_level1 = utility.rf_cmmgr(args['path'], args['server1'], args['user1'], args['password1'], args['port1'], args['project1'], 'Project')
+    project_level2 = utility.rf_cmmgr(args['path'], args['server2'], args['user2'], args['password2'], args['port2'], args['project2'], 'Project')
+
+    
+ 
+
+
+if __name__ == '__main__':
+    main()

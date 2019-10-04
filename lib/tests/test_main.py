18bc44d74da0a3f36d8110a9cd140dd7afbc01c7
codechris1 <52330846+codechris1@users.noreply.github.com>
2019-10-04 04:36:03 +0000

    a
    a

--- lib/tests/test_main.py
+++ lib/tests/test_main.py
@@ -1,7 +1,11 @@
 import utility
+import json
+
+def save_config(args,config_name):
+    with open('conf\\' + config_name + '.json','w+') as jsonfile:
+        json.dump(args, jsonfile, indent=4, separators=(',', ': '), sort_keys=True)
+
 
 args = utility.input_parameters()
 print args
-    
-    #args['path']=raw_input('Provide the Path Locaton: ')
-    #args['server1']=raw_input('Provide the Hostname for Server 1: ')
+save_config(args,'test1')
